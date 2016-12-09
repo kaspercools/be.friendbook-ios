024c19b8c06faf717e54abd60541f6ed8a06bce4
Kasper Cools <kasper@todum.be>
2016-12-09 20:40:30 +0100

    Initial Commit

---
+++ be.friendbook.ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  be.friendbook.ios
+//
+//  Created by Kasper Cools on 09/12/2016.
+//  Copyright © 2016 Kasper Cools. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
